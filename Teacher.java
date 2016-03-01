1790146eafb3f81c3709986fabcd2cab5f0bec9c
m263313 <m263313@users.noreply.github.com>
2016-03-01 22:02:18 +0200

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,14 @@
 public class Teacher{
-public String name;
+private String name;
+public Teacher(String name ){
+	 
+	this.name=name;
+	 
+}
+public Teacher(){
+ this("Teach");
+}
+public String toString(){
+	return "Name "+name;
+}
 }
