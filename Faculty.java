c7b9f5184deafa725bdb403a9dc14422ae3be264
m263313 <m263313@users.noreply.github.com>
2016-03-01 22:00:38 +0200

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -1,4 +1,59 @@
 public class Faculty {
 	private Cathedra[] cahedras = new Cathedra[0];
-public String name;
+	private String name;
+public Faculty(String name){
+	this.name=name;
+}
+public Faculty(){
+	this("Fac");
+}
+private void addCathedra (){
+	Cathedra[] temp = new Cathedra[cahedras.length+1];
+	for(int i=0;i<cahedras.length+1;i++){
+		temp[i]=cahedras[i];
+	}
+	temp[temp.length]= new Cathedra(name );
+	cahedras=temp;
+}
+private void deleteCathedra(){
+	Cathedra[] temp;
+	boolean r=false;
+	for(int i=0;i<cahedras.length;i++){
+		if(cahedras[i].getName().equals(name)){
+			cahedras[i]=null;
+			r=true;
+		}
+		
+	}
+	if(r){
+		  temp = new Cathedra[cahedras.length-1];
+	for(int i=0;i<cahedras.length-1;i++){
+		if(cahedras[i]!=null)
+		temp[i]=cahedras[i];
+	}
+	}
+	else{
+		return;
+	}
+	 	
+	cahedras=temp;
+}
+private void editCathedra(){
+
+}
+private void allStudentCourse(){
+	
+}
+private void allStudentName(){
+	
+}
+private void allTeacherName(){
+	
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
 }
