95199b3a705338342aac0308ef276931f0e1be53
m263313 <m263313@users.noreply.github.com>
2016-03-04 18:53:14 +0200

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,14 +1,21 @@
 public class Teacher{
-private String name;
+protected String name;
 public Teacher(String name ){
 	 
 	this.name=name;
 	 
 }
+
 public Teacher(){
  this("Teach");
 }
 public String toString(){
 	return "Name "+name;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
 }
 }
