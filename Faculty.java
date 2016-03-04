ffe308493fa90a16f87f623c5cb5eaf074f08000
m263313 <m263313@users.noreply.github.com>
2016-03-04 18:59:31 +0200

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -1,53 +1,61 @@
 public class Faculty {
-	private Cathedra[] cahedras = new Cathedra[0];
-	private String name;
+	protected Cathedra[] cathedras = new Cathedra[0];
+	protected String name;
 public Faculty(String name){
 	this.name=name;
 }
 public Faculty(){
 	this("Fac");
 }
-private void addCathedra (){
-	Cathedra[] temp = new Cathedra[cahedras.length+1];
-	for(int i=0;i<cahedras.length+1;i++){
-		temp[i]=cahedras[i];
+public void addCathedra (){
+	Cathedra[] temp = new Cathedra[cathedras.length+1];
+	for(int i=0;i<cathedras.length+1;i++){
+		temp[i]=cathedras[i];
 	}
 	temp[temp.length]= new Cathedra(name );
-	cahedras=temp;
+	cathedras=temp;
 }
-private void deleteCathedra(){
+public void deleteCathedra(String name){
 	Cathedra[] temp;
 	boolean r=false;
-	for(int i=0;i<cahedras.length;i++){
-		if(cahedras[i].getName().equals(name)){
-			cahedras[i]=null;
+	for(int i=0;i<cathedras.length;i++){
+		if(cathedras[i].getName().equals(name)){
+			cathedras[i]=null;
 			r=true;
 		}
 		
 	}
 	if(r){
-		  temp = new Cathedra[cahedras.length-1];
-	for(int i=0;i<cahedras.length-1;i++){
-		if(cahedras[i]!=null)
-		temp[i]=cahedras[i];
+		  temp = new Cathedra[cathedras.length-1];
+	for(int i=0;i<cathedras.length-1;i++){
+		if(cathedras[i]!=null)
+		temp[i]=cathedras[i];
 	}
 	}
 	else{
 		return;
 	}
 	 	
-	cahedras=temp;
+	cathedras=temp;
 }
-private void editCathedra(){
+public void editCathedra(String name,String newName){
+	for(int i =0;i<cathedras.length;i++){
+		if(cathedras[i].getName().equals(name)){
+			cathedras[i].setName(newName);
+			
+		}
+	}
+}
 
-}
-private void allStudentCourse(){
+public void allStudentCourse(){
 	
 }
-private void allStudentName(){
+//Вивести всіх студентів/викладачів факультета впорядкованих за алфавітом.
+public void allStudentName(){
 	
 }
-private void allTeacherName(){
+//Вивести всіх студентів/викладачів факультета впорядкованих за алфавітом.
+public void allTeacherName(){
 	
 }
 public String getName() {
