27d225f3663a5afe1dcbf51152925ea808186f21
m263313 <m263313@users.noreply.github.com>
2016-03-04 18:52:51 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,3 +1,11 @@
 public class Test{
-System.out.println("Lolka");
+	public void main(String[] args){
+		University NaUKMA =new University();
+		int p=1;
+		while(p==1){
+System.out.println("What do you want?");
+//A lot of switches with variants
+
+		}
+}
 }
